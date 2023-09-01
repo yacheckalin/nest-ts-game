a618b7078dd66dc1fb0de95617dfd17976898465
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2023-09-01 11:19:10 +0200

    feat: add games entity/module/controller/service base structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GamesModule } from './games/games.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Games } from './games/games.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    GamesModule,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'sqlite',
+        database: config.get<string>('SQL_DB_NAME'),
+        entities: [Games],
+        synchronize: true,
+      }),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
