c45f96eeb67b0ca9d9b7582c36c769491f78bc27
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2023-09-01 11:41:50 +0200

    feat: add API for updating game by ID

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  Patch,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { GamesService } from './games.service';
 import { CreateGameDto } from './dto/create-game.dto';
 import { Games } from './games.entity';
+import { UpdateGameDto } from './dto/update-game.dto';
 
 @Controller('games')
 export class GamesController {
@@ -10,5 +18,13 @@ export class GamesController {
   @Post()
   createNewGame(@Body() body: CreateGameDto): Promise<Games> {
     return this.gamesService.createGame(body);
+  }
+
+  @Patch('/:id')
+  updateGameById(
+    @Param('id', new ParseIntPipe()) id: number,
+    @Body() body: Partial<UpdateGameDto>,
+  ): Promise<Games> {
+    return this.gamesService.updateGame(+id, body);
   }
 }
--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Games } from './games.entity';
 import { CreateGameDto } from './dto/create-game.dto';
+import { UpdateGameDto } from './dto/update-game.dto';
 
 @Injectable()
 export class GamesService {
@@ -13,5 +14,15 @@ export class GamesService {
   async createGame(body: CreateGameDto): Promise<Games> {
     const game = await this.repo.create({ createdAt: new Date(), ...body });
     return this.repo.save(game);
+  }
+
+  async updateGame(id: number, body: Partial<UpdateGameDto>): Promise<Games> {
+    const game = await this.repo.findOne({ where: { id } });
+
+    if (!game) {
+      throw new NotFoundException('There is no game with such ID');
+    }
+    const updatedGame = { ...game, ...body };
+    return this.repo.save(updatedGame);
   }
 }
